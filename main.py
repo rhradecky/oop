a259eb64aa1b99c094a37627349b74ed9a468e50
PY-TU-10-23 <rhradecky@gmail.com>
2024-01-09 21:11:23 +0100

    4.cast  pridali sme dedenie

--- main.py
+++ main.py
@@ -1,30 +1,61 @@
+
 class Person:
-    def __init__(self, name, age, gender, occupation, color):
-        self.__name =  name
+    def __init__(self, name, age, gender, occupation):
+        self.name =  name
         self.age = age
         self.gender = gender
-        self.occupation = occupation
-        self.color = color
+        self.__occupation = occupation
+
+
+
+
 
     def pozdrav(self):
-        print(f"Ahoj volam sa {self.__name} a mam {self.age} rokov")
+        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov")
 
     def bydlisko(self):
-        print(f"Ahoj volam sa {self.__name} a byvam v  {self.occupation}")
-
-    def zostarni(self, kolko):
-        self.age += 5
+        print(f"Ahoj volam sa {self.name} a byvam v  {self.__occupation}")
 
 
-richard = Person( "Richard", 30, "muz", "Ivanka", "modra")
+class Student(Person):
+    def __init__(self, name, age, gender,occupation, score):
+        super().__init__(name, age, gender, occupation)
+        self.score = score
+
+    def jeGenius(self):
+        if self.score > 90:
+            print(f"{self.name} je genius a {self.gender}")
+        else:
+            print(f"{self.name} nie je genius a {self.gender}")
 
 
-print(richard.pozdrav())
 
-richard.__name = "Fero"
+patrik = Student( "Patrik", 30, "muz", "Bratislava", 95)
+milan = Student( "Milan", 30, "muz", "Bratislava", 48)
 
-print(richard.pozdrav())
-#richard.zostarni(5)
-#print(richard.color)
 
-print(richard.bydlisko())
+print(patrik.name)
+patrik.pozdrav()
+patrik.jeGenius()
+milan.jeGenius()
+
+
+
+'''
+class City:
+    def __init__(self, city, region, country, numbOfCityzens, zip ):
+        self.city =  city
+        self.region = region
+        self.country = country
+        self.numbOfCityzens = numbOfCityzens
+        self.zip = zip
+
+    def pozdrav(self):
+        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov")
+
+
+Bratislava = City( "Bratislava", "Zapadoslovensky", "Slovensko", 500000, 95, 84201)
+
+print
+
+'''
