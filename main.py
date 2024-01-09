80dbb70e622239068250c99928dddbe5cf2d0402
PY-TU-10-23 <rhradecky@gmail.com>
2024-01-09 20:20:10 +0100

    1.cast

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 class Person:
-    def __init__(self, name, age, gender, occupation):
-        self.name =  name
+    def __init__(self, name, age, gender, occupation, color):
+        self.__name =  name
         self.age = age
         self.gender = gender
         self.occupation = occupation
-
+        self.color = color
 
     def pozdrav(self):
-        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov")
+        print(f"Ahoj volam sa {self.__name} a mam {self.age} rokov")
 
     def bydlisko(self):
-        print(f"Ahoj volam sa {self.name} a byvam v  {self.occupation} ")
+        print(f"Ahoj volam sa {self.__name} a byvam v  {self.occupation}")
 
     def zostarni(self, kolko):
         self.age += 5
 
 
-richard = Person( "Richard", 30, "technik", "Ivanka")
-
-
+richard = Person( "Richard", 30, "muz", "Ivanka", "modra")
 
 
 print(richard.pozdrav())
-richard.zostarni(5)
+
+richard.__name = "Fero"
+
 print(richard.pozdrav())
+#richard.zostarni(5)
+#print(richard.color)
+
+
+print(richard.bydlisko())
